0681a05b6a55845a609e17caa92e6e4f485104ff
aravindkarri <aravindkarri98495@gmail.com>
2021-06-21 21:22:54 +0530

    Added: code to check full time and part time employee daily wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,21 +4,31 @@ public class EmployeeWage {
 	
 	public static void main(String args[]) {
 		
-		int IS_EMP_PRESENT = 1;
-		int EMP_HRS = 8;
+		//constants
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
+		//variables
 		int dailyWage = 0;
+		int empHrs=0;
+		
 		System.out.println("Welcome to Employee Computation Program on Master Branch");
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_EMP_PRESENT)
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
 		{
-			System.out.println("Employee is present");
-			dailyWage = (EMP_HRS * EMP_RATE_PER_HR);
-			System.out.println("Employee daily wage is: " +dailyWage);
+			System.out.println("Employee is full time present");
+			empHrs=8;
+		}
+		else if (empCheck == IS_PART_TIME)
+		{
+			System.out.println("Employee is part time present");
+			empHrs=4;
 		}
 		else
 		{
 			System.out.println("Employee is absent");
 		}
+		dailyWage=(empHrs*EMP_RATE_PER_HR);
+		System.out.println("Employee daily wage is "+dailyWage);
 	}
 }
