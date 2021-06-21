8f4b65eea63af28a543fe8789b761f526994d053
aravindkarri <aravindkarri98495@gmail.com>
2021-06-21 21:09:33 +0530

    Added: code to check employee dailywage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,16 +5,20 @@ public class EmployeeWage {
 	public static void main(String args[]) {
 		
 		int IS_EMP_PRESENT = 1;
+		int EMP_HRS = 8;
+		int EMP_RATE_PER_HR = 20;
+		int dailyWage = 0;
 		System.out.println("Welcome to Employee Computation Program on Master Branch");
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_EMP_PRESENT)
 		{
 			System.out.println("Employee is present");
+			dailyWage = (EMP_HRS * EMP_RATE_PER_HR);
+			System.out.println("Employee daily wage is: " +dailyWage);
 		}
 		else
 		{
 			System.out.println("Employee is absent");
 		}
-		
 	}
 }
